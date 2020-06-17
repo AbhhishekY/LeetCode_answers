e2fd8f1234a964c17850e133ac9d0614dbabc8da
Abhishek Yadav <abhhishek.yaadav@gmail.com>
2020-06-17 22:41:32 +0530

    Update and rename Vlidate Binary Search Tree to Vlidate Binary Search Tree.cpp

--- Vlidate Binary Search Tree.cpp
+++ Vlidate Binary Search Tree.cpp
@@ -1,5 +1,5 @@
-98. Validate Binary Search Tree
-https://leetcode.com/problems/validate-binary-search-tree/
+// 98. Validate Binary Search Tree
+// https://leetcode.com/problems/validate-binary-search-tree/
 
  vector<int> res;
     void helper(TreeNode* root) {
